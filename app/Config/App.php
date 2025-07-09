e6cbaba27497c807e7a744465007a410eb0ac05e
KillerKing93 <alifnurhidayatwork@gmail.com>
2025-07-09 20:00:39 +0700

    update app baseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://shop-website.test/';
+    public string $baseURL = 'https://shop-website.craftthingy.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
